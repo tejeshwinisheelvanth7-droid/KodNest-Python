bce0bf37e6cffb10c8cbecf82e2135657c59c3fe
tejeshwini <tejeshwinisheelvanth7@gmail.com>
2026-08-05 21:58:24 +0530

    day4changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/m2(2).py
@@ -0,0 +1,7 @@
+marks = int(input())
+attendance = int(input())
+project_completion_status = input()
+if marks >=60 and attendance >=75:
+    if project_completion_status =="Yes":
+        print("Eligible")
+print("Not Eligible")
