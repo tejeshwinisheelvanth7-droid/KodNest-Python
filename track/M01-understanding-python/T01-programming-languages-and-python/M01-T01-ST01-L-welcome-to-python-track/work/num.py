bcb1a92d0afef3907961b1db958eab69fdfecf16
tejeshwini <tejeshwinisheelvanth7@gmail.com>
2026-08-07 02:02:04 +0530

    day1changes

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/num.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/num.py
@@ -1,4 +1,4 @@
-first_number = int(input())
-second_number = int(input())
-print(first_number / second_number)
-print(first_number // second_number)
+first_number = int()
+second_number = int()
+print(first_number/second_number)
+print(first_number//second_number)
--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/result.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/result.py
@@ -1,4 +1,4 @@
-a = int(input())
-b = int(input())
+a = int()
+b = int()
 sum_result = a + b
 print(f"sum: {sum_result}")
